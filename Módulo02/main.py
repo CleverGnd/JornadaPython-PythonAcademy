87797f50b7205d6df33e8c341d32ab4b053f37b5
Cleverson Guandalin <clever@unochapeco.edu.br>
2022-09-19 23:12:12 -0300

    Módulo 02 - Concluído

---
+++ Módulo02/main.py
@@ -0,0 +1,28 @@
+x = 'String'
+
+# O "Todo" pode ser utilizado como um lembrete, todos ficam em destaque e aparecem na aba do TODO abaixo
+# Todo: Melhorar este print
+print('Jornada Python está começando!')
+
+# DICA: Ctrl + R = Localizar e substituir
+
+print(x) #Trocar variavel com mesmo nome (refactor)
+print(x)
+
+# DICA: Shift + F10 = Rodar 'RUN' o código
+# DICA: Shift + F9 = Debug
+# DICA: Ctrl + D = Duplicar item indicado pelo cursor
+# DICA: Ctrl + Alt + T = PyCharm sugere blocos para criar
+
+# Primeira aplicação:
+
+print('Hello World!')
+
+# Palavras reservadas do sistema
+# if
+# while
+# .... Não podem ser utilizadas como variáveis do programa
+
+if True:
+    # indentação
+print('Jornada Python')
