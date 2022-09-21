33624da0179f323754a8aa6b1854357c6b473693
Cleverson Guandalin <clever@unochapeco.edu.br>
2022-09-20 23:58:14 -0300

    Módulo 03 - Concluído

---
+++ Módulo03/Variáveis.py
@@ -0,0 +1,32 @@
+# Variáveis
+
+primeira_variavel = 5
+print(primeira_variavel)
+print(type(primeira_variavel))  # Retorna o tipo da variável
+
+primeira_variavel = 'string'
+print(primeira_variavel)
+print(type(primeira_variavel))
+
+variavel_1 = 1
+
+
+# Tipos de Dados
+
+nome = 'clever'
+sobrenome = 'gnd'
+
+print(nome + sobrenome.capitalize())  # capitalize deixa a primeira letra em Maiúsculo
+
+idade = 30
+altura = 1.8
+print(f'Meu nome é {nome.capitalize()} {sobrenome.capitalize()}. Minha idade é {idade} anos e meço {altura} m')
+
+numero_complexo = complex(1, 4)
+print(numero_complexo)
+
+condicao = False
+print(condicao)
+
+
+
