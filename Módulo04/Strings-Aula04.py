b5691d5d2abbd639f7db6700c86f090dac75d002
Cleverson Guandalin <clever@unochapeco.edu.br>
2022-10-15 15:15:38 -0300

    Módulo 03 - Aula 04

---
+++ Módulo04/Strings-Aula04.py
@@ -0,0 +1,8 @@
+# Funções de strings
+print('   Strip   '.strip())  # Remove espeços
+print('maiúsculo'.upper())
+print('MINÚSCULO'.lower())
+print('T,exto, c,o,m vá,ria,s v,ír,g,ulas'.replace(',', ''))
+print('Texto teste para a função count'.count('e'))
+print('Texti Cebtrakuzadi'.center(50, '*'))
+print('Avião?'.index('ã'))
