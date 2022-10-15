80469fbb5987d9884e7eead12e2ea1bb4887809a
Cleverson Guandalin <clever@unochapeco.edu.br>
2022-10-15 16:06:41 -0300

    Módulo 05 - Aula 01

---
+++ Módulo05/Numéricos-Aula01.py
@@ -0,0 +1,12 @@
+numero_1 = 21
+numero_2 = 100
+numero_3 = 0.5
+
+print(numero_1 + numero_2)
+print(numero_1 - numero_2)
+print(numero_1 * numero_2)
+print(numero_1 / numero_2)
+print(numero_3 * (numero_1 + numero_2))
+print(numero_2 % numero_1) # Módulo - resto da divisão
+print(numero_1 ** 3) # Potenciação ^3
+print(numero_1 // 5) # 4.2 - Divisão arredondada para baixo
