cfcbf6ac4aac277190c1b0f0e600d0ee74c3d8aa
Cleverson Guandalin <clever@unochapeco.edu.br>
2022-10-16 16:48:27 -0300

    Módulo 05 - Aula 03

---
+++ Módulo05/Numéricos-Aula03.py
@@ -0,0 +1,12 @@
+n0 = 50
+n1 = -50
+print(abs(n0)) # Abs retorna o valor absoluto
+print(abs(n1))
+
+numero = 5.65645
+print(round(numero)) # Round arrendonda número, após virgula numero de casas decimais
+print(round(numero, 3))
+
+numero1 = 5
+print(pow(numero1, 2)) # Pow exponenciação, após vírgula é o expoente
+print(pow(numero1, 2, 2)) # segunda virgula é módulo do valor da exponenciação
