f5d49ed810b282c5c313a4c0ce29b499b5f6e453
Cleverson Guandalin <clever@unochapeco.edu.br>
2022-10-16 16:55:01 -0300

    Módulo 05 - Concluído

---
+++ Módulo05/Numéricos-Aula04.py
@@ -0,0 +1,7 @@
+import math
+# Traz o módulo math (funções matemáticas) para dentro do script
+
+n1 = 5.1224455
+
+print(math.ceil(n1)) # arrendonda para cima um determibado valor
+print(math.floor(n1)) # arrendonda para baixo um determinado valor
