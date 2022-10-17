b165fb262ea6c77acebfa4ddadd97bc347095a6a
Cleverson Guandalin <clever@unochapeco.edu.br>
2022-10-16 23:22:06 -0300

    Módulo 07 - Aula 03

---
+++ Módulo07/ControlStructures-Aula03.py
@@ -0,0 +1,10 @@
+# for
+
+sequencia = [1, 2, 3]
+
+for elemento in sequencia:
+    print(elemento)
+
+# Range
+for i in range(0, 11, 2): # Considera até 10, não inclui o 11
+    print(f'O valor de i é {i}')
