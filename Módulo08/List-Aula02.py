acdd4972c428cb3cd64177b3e4ae638a0ecaef5d
Cleverson Guandalin <clever@unochapeco.edu.br>
2022-10-17 23:16:08 -0300

    Módulo 08 - Aula 02

---
+++ Módulo08/List-Aula02.py
@@ -0,0 +1,23 @@
+pares = [2, 4, 6, 8]
+impares = [1, 3, 5, 7]
+
+print(pares[0])
+print(pares[0] * impares[1])
+print(pares[0] ** impares[2])
+print(f'O segundo elemento da lista \'pares\' é {pares[1]}.')
+
+print(pares[-1]) # Retoma o ultimo elemento
+print(pares[-1] * impares[-1])
+
+print(pares[0])
+print(pares[1])
+print(pares[2])
+print(pares[3])
+print(pares[-1])
+print(pares[-2])
+print(pares[-3])
+print(pares[-4])
+
+print(pares[8]) # Elemento inexistente "list index out of range"
+
+
